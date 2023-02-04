468a4e1cf261979ae8c14fe7769959ceb40723bc
amir-coder <ja_amir@esi.dz>
2023-02-04 12:44:46 +0100

    added roles types hr admin and member

---
+++ src/models/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const db = {};
+
+db.mongoose = mongoose;
+
+db.user = require("./user.model");
+db.role = require("./role.model");
+
+db.ROLES = ["hr", "member", "admin"];
+
+module.exports = db;
